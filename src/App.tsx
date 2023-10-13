fee8dc70ef8d9a4f5d9176059acf631d5e3f642f
Saim Alam <saimalam.work@gmail.com>
2023-10-12 20:57:26 -0400

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import List from "./components/List";
+import Details from "./components/Details";
+import { Routes, Route, Link, Outlet, useParams } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Nav />
-      <List />
+      <Routes>
+        <Route path="/" element={<List />} />
+        <Route path="details/:id" element={<Details />} />
+      </Routes>
     </>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,6 +8,11 @@ const List = () => {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   );
